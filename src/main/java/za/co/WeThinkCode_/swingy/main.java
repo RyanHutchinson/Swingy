22f69799d3bf27691ce1ec4cacb952a10b361928
RyanHutchinson <rhutchin@student.wethinkcode.co.za>
2020-06-29 13:26:15 +0200

    getting environment/packages setup

--- src/main/java/za/co/WeThinkCode_/swingy/main.java
+++ src/main/java/za/co/WeThinkCode_/swingy/main.java
@@ -2,6 +2,17 @@ package za.co.WeThinkCode_.swingy;
 
 public class main{
     public static void main(String[] args) {
-        
+
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("-console")) {
+                System.out.println("go to console control");
+            } else if (args[0].equalsIgnoreCase("-gui")) {
+                System.out.println("go to gui control");
+            } else {
+                System.out.println("Invalid command.\nPlease use a valid flag: [-gui , -console]");
+            }
+        } else {
+            System.out.println("Invalid command.\nPlease use a valid flag: [-gui , -console]");
+        }
     }
 }
