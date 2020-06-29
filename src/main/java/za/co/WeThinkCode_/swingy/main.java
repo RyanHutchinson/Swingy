fafb709694065593203caf25aeea0c61d5ade9bd
RyanHutchinson <rhutchin@student.wethinkcode.co.za>
2020-06-29 16:37:06 +0200

    Done for the day

--- src/main/java/za/co/WeThinkCode_/swingy/main.java
+++ src/main/java/za/co/WeThinkCode_/swingy/main.java
@@ -1,18 +1,23 @@
 package za.co.WeThinkCode_.swingy;
 
+import za.co.WeThinkCode_.swingy.control.Controller;
+
+import javax.naming.ldap.Control;
+
 public class main{
     public static void main(String[] args) {
-
+        Controller GameControl;
         if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("-console")) {
-                System.out.println("go to console control");
-            } else if (args[0].equalsIgnoreCase("-gui")) {
-                System.out.println("go to gui control");
+            if (args[0].equalsIgnoreCase("-console") || args[0].equalsIgnoreCase("-gui")) {
+                GameControl = new Controller(args[0]);
+                GameControl.whatIsView();
             } else {
                 System.out.println("Invalid command.\nPlease use a valid flag: [-gui , -console]");
+                System.exit(0);
             }
         } else {
             System.out.println("Invalid command.\nPlease use a valid flag: [-gui , -console]");
+            System.exit(0);
         }
     }
 }
